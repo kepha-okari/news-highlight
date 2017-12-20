36d21de88a0a68276f34823fd69e3ed0d6d97cca
rkepha <kephaokari@gmail.com>
2017-12-20 18:01:44 +0300

    fixed the assertion errors in unittests

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -1,5 +1,5 @@
 import unittest
-from models import Articles
+from app.models import Articles
 
 class ArticleTest(unittest.TestCase):
     '''
@@ -13,7 +13,7 @@ class ArticleTest(unittest.TestCase):
         '''
         Set up method to run before each test case
         '''
-        self.new_article = Articles('the-next-web','PlayStation\'s $30 PS4 gamepad for kids is totally adorable', 'https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/10/PS4-Mini-gamepad-social.jpg', 'Sony\'s teamed up with Hori on its new $30 Mini Wired Gamepad, which is designed for younger PS4 players with smaller hands.', 'https://thenextweb.com/gaming/2017/10/19/playstations-30-ps4-gamepad-for-kids-is-totally-adorable/', '2017-10-19T13:00:00Z')
+        self.new_article = Articles('BBC News','EU acts against Poland judiciary reforms', 'Unprecedented disciplinary measures are taken as the EU says the reforms threaten the rule of law.', 'https://ichef.bbci.co.uk/news/1024/cpsprodpb/F046/production/_98901516_2efffed4-d4a6-486a-8a78-112232b92faa.jpg','http://www.bbc.co.uk/news/world-europe-42420150', '2017-12-20T13:36:14Z')
 
     def test_instance(self):
         '''
@@ -25,19 +25,19 @@ class ArticleTest(unittest.TestCase):
         '''
         Test case to check if the Article class is initialised
         '''
-        self.assertEqual( self.new_article.author, 'the-next-web')
-        self.assertEqual( self.new_article.title, 'PlayStation\'s $30 PS4 gamepad for kids is totally adorable')
-        self.assertEqual( self.new_article.urlToImage, 'https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/10/PS4-Mini-gamepad-social.jpg')
-        self.assertEqual( self.new_article.description, 'Sony\'s teamed up with Hori on its new $30 Mini Wired Gamepad, which is designed for younger PS4 players with smaller hands.')
-        self.assertEqual( self.new_article.urlToArticle, 'https://thenextweb.com/gaming/2017/10/19/playstations-30-ps4-gamepad-for-kids-is-totally-adorable/')
-        self.assertEqual( self.new_article.publishedAt, '2017-10-19T13:00:00Z')
+        self.assertEqual( self.new_article.author, 'BBC News')
+        self.assertEqual( self.new_article.title, 'EU acts against Poland judiciary reforms')
+        self.assertEqual( self.new_article.description, 'Unprecedented disciplinary measures are taken as the EU says the reforms threaten the rule of law.')
+        self.assertEqual( self.new_article.urlToImage, 'https://ichef.bbci.co.uk/news/1024/cpsprodpb/F046/production/_98901516_2efffed4-d4a6-486a-8a78-112232b92faa.jpg')
+        self.assertEqual( self.new_article.url, 'http://www.bbc.co.uk/news/world-europe-42420150')
+        self.assertEqual( self.new_article.publishedAt, '2017-12-20T13:36:14Z')
 
     def test_publish_date_format(self):
         '''
         Test case to check if UTC date format is converted to a display-friendly format
         '''
         display_friendly_format = self.new_article.publish_date_format(self.new_article.publishedAt)
-        self.assertEqual( display_friendly_format, '2017-10-19')
+        self.assertEqual( display_friendly_format, '2017-12-20')
 
 
 
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,6 +1,6 @@
 import unittest
 # from app import app
-from models import Sources
+from app.models import Sources
 
 class SourcesTest(unittest.TestCase):
     '''
